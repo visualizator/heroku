2a11bb2f39dabc88443314740817580b7e862514
visualizator <visualizator@naver.com>
2015-12-04 11:42:01 +0900

    정상수정

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-﻿console.log('Hello World .. !');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
